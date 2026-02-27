545961f5f5b782f091c85159d43a2026a102b126
Ameerlog <testbatteryfy@gmail.com>
2026-02-27 12:29:38 +0530

    file services.java added base url

--- src/main/java/com/ai/cobrother/Service/FileService.java
+++ src/main/java/com/ai/cobrother/Service/FileService.java
@@ -1,39 +1,3 @@
-//package com.ai.cobrother.Service;
-//
-//import org.springframework.stereotype.Service;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.File;
-//import java.nio.file.Files;
-//import java.nio.file.Path;
-//import java.nio.file.Paths;
-//import java.util.UUID;
-//
-//@Service
-//public class FileService {
-//
-//    private final String uploadDir = "uploads/";
-//
-//    public String uploadFile(MultipartFile file) throws Exception {
-//
-//        File dir = new File(uploadDir);
-//        if (!dir.exists()) {
-//            dir.mkdirs();
-//        }
-//
-//        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-//        Path filePath = Paths.get(uploadDir, fileName);
-//
-//        Files.copy(file.getInputStream(), filePath);
-//
-//        return "http://localhost:8080/uploads/" + fileName;
-//    }
-//}
-
-
-
-
-
 package com.ai.cobrother.Service;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -52,6 +16,9 @@ public class FileService {
     @Value("${file.upload-dir}")
     private String uploadDir;
 
+    @Value("${app.base-url:https://cobrother-api.onrender.com}")
+    private String uploadedFileBaseUrl;
+
     public String uploadFile(MultipartFile file) throws Exception {
 
         File dir = new File(uploadDir);
@@ -65,6 +32,7 @@ public class FileService {
         Files.copy(file.getInputStream(), filePath);
 
 //        return "http://localhost:8080/uploads/" + fileName;
-        return "http://192.168.29.184:8080/uploads/" + fileName;
+        // return "http://192.168.29.184:8080/uploads/" + fileName;
+       return uploadedFileBaseUrl + "/uploads/" + fileName;
     }
 }
