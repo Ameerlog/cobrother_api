df3100eeb5a77fc74d47f0b6c6bf894ec8a54928
Ameerlog <testbatteryfy@gmail.com>
2026-02-27 13:05:30 +0530

    Changed cobrotherapplication.java file

--- src/main/java/com/ai/cobrother/CobrotherApplication.java
+++ src/main/java/com/ai/cobrother/CobrotherApplication.java
@@ -1,38 +1,13 @@
 package com.ai.cobrother;
 
-import com.ai.cobrother.Model.User;
-import com.ai.cobrother.Repository.UserRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class  CobrotherApplication {
+public class CobrotherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CobrotherApplication.class, args);
-
-
-//        System.out.println("ENV = " + System.getenv("LINKEDIN_CLIENT_ID"));
-    }
-
-
-    @Bean
-    CommandLineRunner test(UserRepository repo){
-        return args -> {
-            User u = new User();
-            u.setUsername("demoUser");
-            u.setPassword("123456");
-            repo.save(u);
-        };
-    }
-
-    @Bean
-    CommandLineRunner checkDB(org.springframework.data.mongodb.core.MongoTemplate template){
-        return args -> {
-            System.out.println("Connected DB: " + template.getDb().getName());
-        };
     }
 
 }
